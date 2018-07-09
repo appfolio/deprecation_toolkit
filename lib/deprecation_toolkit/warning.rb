bbac66dce4a5ec4ec4bce8ff0aab3daba4e7e207
Edouard CHIN <edouard.chin@shopify.com>
2018-07-09 18:46:29 -0400

    Monkeypatching Kernel#warn is non trivial:
    - The previous solution didn't work, I didn't test it properly
    - The Warning module was introduced in ruby 2.4.0, but it had a bug because `Kernel.warn` isn't delegating to
      `Warning.warn`. That bug was fixed on ruby 2.5 . In order to make this work we need to create a workaround
      and monkeypatch `Kernel.warn` to call `Warning.warn`
      `Kernel.warn` accepts any number of arguments, but `Warning.warn` only accepts a single (it gets converted
      to a single string joined with new lines). The ruby specs has a similar monkey patch
      https://github.com/ruby/ruby/blob/d8583b132c4ce3c203c9f5a117fd581c8c7eb6fe/spec/mspec/lib/mspec/utils/warnings.rb#L10-L12

--- lib/deprecation_toolkit/warning.rb
+++ lib/deprecation_toolkit/warning.rb
@@ -2,32 +2,35 @@
 
 module DeprecationToolkit
   module Warning
-    def warn(str)
+    extend self
+
+    def deprecation_triggered?(str)
       if DeprecationToolkit::Configuration.warnings_treated_as_deprecation.any? { |warning| warning =~ str }
         ActiveSupport::Deprecation.warn(str)
-      else
-        super
+        true
       end
     end
   end
 end
 
-if defined?(Warning)
-  Warning.singleton_class.prepend(DeprecationToolkit::Warning)
+# Warning is a new feature in ruby 2.5
+module Warning
+  def warn(str)
+    super unless DeprecationToolkit::Warning.deprecation_triggered?(str)
+  end
 end
 
-# https://bugs.ruby-lang.org/issues/12944
-if RUBY_VERSION <= '2.5.0' && RUBY_ENGINE == 'ruby'
+# Support for version older < 2.5
+# Note that the `Warning` module exists in Ruby 2.4 but has a bug https://bugs.ruby-lang.org/issues/12944
+if RUBY_VERSION < '2.5.0' && RUBY_ENGINE == 'ruby'
   module Kernel
+    alias_method :__original_warn, :warn
+
     def warn(*messages)
-      Array(messages.flatten).each do |str|
-        if DeprecationToolkit::Configuration.warnings_treated_as_deprecation.any? { |warning| warning =~ str }
-          ActiveSupport::Deprecation.warn(str)
-        else
-          _original_warn
-        end
-      end
+      message = messages.join("\n")
+      message += "\n" unless message.end_with?("\n")
+
+      __original_warn(messages) unless DeprecationToolkit::Warning.deprecation_triggered?(message)
     end
-    alias_method :_original_warn, :warn
   end
 end
