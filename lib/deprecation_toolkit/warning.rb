534616729f930a405276e8da9c2dcc84f7d47944
Pete Kinnecom <pete.kinnecom@appfolio.com>
2018-07-10 15:12:59 -0700

    remove warning (we don't need it)

--- lib/deprecation_toolkit/warning.rb
+++ lib/deprecation_toolkit/warning.rb
@@ -1,26 +1,40 @@
 # frozen_string_literal: true
 
-module DeprecationToolkit
-  module Warning
-    def warn(str)
-      if DeprecationToolkit::Configuration.warnings_treated_as_deprecation.any? { |warning| warning =~ str }
-        ActiveSupport::Deprecation.warn(str)
-      else
-        super
-      end
-    end
-  end
-end
+# module DeprecationToolkit
+#   module Warning
+#     def warn(str)
+#       if DeprecationToolkit::Configuration.warnings_treated_as_deprecation.any? { |warning| warning =~ str }
+#         ActiveSupport::Deprecation.warn(str)
+#       else
+#         super
+#       end
+#     end
+#   end
+# end
 
-Warning.singleton_class.prepend(DeprecationToolkit::Warning)
+# if !defined?(Warning)
+#   class Warning
+#     def warn(*a)
+#       Kernel.warn(*a)
+#     end
 
-# https://bugs.ruby-lang.org/issues/12944
-if RUBY_VERSION <= '2.5.0' && RUBY_ENGINE == 'ruby'
-  module Kernel
-    remove_method :warn
+#     class << self
+#       def warn(*a)
+#         Kernel.warn(*a)
+#       end
+#     end
+#   end
+# end
 
-    def warn(*messages)
-      Array(messages.flatten).each { |msg| Warning.warn(msg) }
-    end
-  end
-end
+# Warning.singleton_class.prepend(DeprecationToolkit::Warning)
+
+# # https://bugs.ruby-lang.org/issues/12944
+# if RUBY_VERSION <= '2.5.0' && RUBY_ENGINE == 'ruby'
+#   module Kernel
+#     remove_method :warn
+
+#     def warn(*messages)
+#       Array(messages.flatten).each { |msg| Warning.warn(msg) }
+#     end
+#   end
+# end
--- test/deprecation_toolkit/warning_test.rb
+++ test/deprecation_toolkit/warning_test.rb
@@ -1,51 +1,51 @@
 # frozen_string_literal: true
 
-require "test_helper"
+# require "test_helper"
 
-module DeprecationToolkit
-  class WarningTest < ActiveSupport::TestCase
-    setup do
-      @previous_warnings_treated_as_deprecation = Configuration.warnings_treated_as_deprecation
-    end
+# module DeprecationToolkit
+#   class WarningTest < ActiveSupport::TestCase
+#     setup do
+#       @previous_warnings_treated_as_deprecation = Configuration.warnings_treated_as_deprecation
+#     end
 
-    teardown do
-      Configuration.warnings_treated_as_deprecation = @previous_warnings_treated_as_deprecation
-    end
+#     teardown do
+#       Configuration.warnings_treated_as_deprecation = @previous_warnings_treated_as_deprecation
+#     end
 
-    test 'treats warnings as deprecations' do
-      Configuration.warnings_treated_as_deprecation = [/#example is deprecated/]
+#     test 'treats warnings as deprecations' do
+#       Configuration.warnings_treated_as_deprecation = [/#example is deprecated/]
 
-      assert_raises Behaviors::DeprecationIntroduced do
-        warn '#example is deprecated'
+#       assert_raises Behaviors::DeprecationIntroduced do
+#         warn '#example is deprecated'
 
-        trigger_deprecation_toolkit_behavior
-      end
-    end
+#         trigger_deprecation_toolkit_behavior
+#       end
+#     end
 
-    test 'warn can be called with an array' do
-      Configuration.warnings_treated_as_deprecation = [/#example is deprecated/, /#something is deprecated/]
+#     test 'warn can be called with an array' do
+#       Configuration.warnings_treated_as_deprecation = [/#example is deprecated/, /#something is deprecated/]
 
-      error = assert_raises Behaviors::DeprecationIntroduced do
-        warn ['#example is deprecated', '#something is deprecated']
+#       error = assert_raises Behaviors::DeprecationIntroduced do
+#         warn ['#example is deprecated', '#something is deprecated']
 
-        trigger_deprecation_toolkit_behavior
-      end
+#         trigger_deprecation_toolkit_behavior
+#       end
 
-      assert_match(/DEPRECATION WARNING: #example is deprecated/, error.message)
-      assert_match(/DEPRECATION WARNING: #something is deprecated/, error.message)
-    end
+#       assert_match(/DEPRECATION WARNING: #example is deprecated/, error.message)
+#       assert_match(/DEPRECATION WARNING: #something is deprecated/, error.message)
+#     end
 
-    test 'warn can be called with multiple arguments' do
-      Configuration.warnings_treated_as_deprecation = [/#example is deprecated/, /#something is deprecated/]
+#     test 'warn can be called with multiple arguments' do
+#       Configuration.warnings_treated_as_deprecation = [/#example is deprecated/, /#something is deprecated/]
 
-      error = assert_raises Behaviors::DeprecationIntroduced do
-        warn '#example is deprecated', '#something is deprecated'
+#       error = assert_raises Behaviors::DeprecationIntroduced do
+#         warn '#example is deprecated', '#something is deprecated'
 
-        trigger_deprecation_toolkit_behavior
-      end
+#         trigger_deprecation_toolkit_behavior
+#       end
 
-      assert_match(/DEPRECATION WARNING: #example is deprecated/, error.message)
-      assert_match(/DEPRECATION WARNING: #something is deprecated/, error.message)
-    end
-  end
-end
+#       assert_match(/DEPRECATION WARNING: #example is deprecated/, error.message)
+#       assert_match(/DEPRECATION WARNING: #something is deprecated/, error.message)
+#     end
+#   end
+# end
